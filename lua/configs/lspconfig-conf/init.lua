2d1242b05463e67041d8fce28c67ffba0babe331
Stephan Randle <stephanrandle@hotmail.com>
2024-03-17 00:26:53 -0400

    changed format code keymap

--- lua/configs/lspconfig-conf/init.lua
+++ lua/configs/lspconfig-conf/init.lua
@@ -129,7 +129,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<leader>fm", function()
+		vim.keymap.set("n", "<leader>fc", function()
 			vim.lsp.buf.format({ async = true })
 		end, opts)
 	end,
