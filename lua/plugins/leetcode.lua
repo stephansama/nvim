2c598f8acc8546cdacd5b1ab7d1509bd3ed5743b
Stephan Randle <stephanrandle@hotmail.com>
2024-04-07 15:52:51 -0400

    updated leetcode invokation

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,13 +1,11 @@
 return {
 	"kawre/leetcode.nvim",
 	build = ":TSUpdate html",
-	keys = { { "le", ":Leet<cr>", desc = "Open leet" } },
-	opts = {},
+	keys = { { "le", "<cmd>Leet<cr>", desc = "Open leet" } },
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim", -- required by telescope
 		"MunifTanjim/nui.nvim",
-
 		-- optional
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
