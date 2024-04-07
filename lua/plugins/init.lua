4e7a60f50ce217eb7fe6db3d110ca3c2c25107e8
Stephan Randle <stephanrandle@hotmail.com>
2024-04-07 15:44:00 -0400

    removed from init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,22 +7,6 @@ return {
 		end,
 	},
 	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<C-h>", "<CMD><C-U>TmuxNavigateLeft<CR>" },
-			{ "<C-j>", "<CMD><C-U>TmuxNavigateDown<CR>" },
-			{ "<C-k>", "<CMD><C-U>TmuxNavigateUp<CR>" },
-			{ "<C-l>", "<CMD><C-U>TmuxNavigateRight<CR>" },
-		},
-	},
-	{
 		"axieax/urlview.nvim",
 		config = true,
 		keys = { { "<leader>gx", "<cmd>UrlView<cr>", desc = "Open urlview" } },
