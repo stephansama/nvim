7b186fea80029b3deaecfa2068954db0e314be45
Stephan Randle <stephanrandle@hotmail.com>
2024-05-17 07:54:51 -0400

    refactored snippets to new file

--- lua/plugins/editor/cmp.lua
+++ lua/plugins/editor/cmp.lua
@@ -1,32 +1,3 @@
---- Load snippets from various sources
----@param _ unknown discard
----@param opts table table of options
-local load_snippets = function(_, opts)
-	local luasnip, from_vscode, from_snipmate, from_lua =
-		require("luasnip"),
-		require("luasnip.loaders.from_vscode"),
-		require("luasnip.loaders.from_snipmate"),
-		require("luasnip.loaders.from_lua")
-
-	luasnip.config.set_config(opts)
-
-	from_vscode.lazy_load({ paths = { "./snippets" } })
-
-	from_snipmate.load()
-	from_snipmate.lazy_load({ paths = vim.g.snipmate_snippets_path or "" })
-
-	from_lua.load()
-	from_lua.lazy_load({ paths = vim.g.lua_snippets_path or "" })
-
-	vim.api.nvim_create_autocmd("InsertLeave", {
-		callback = function()
-			if luasnip.session.current_nodes[vim.api.nvim_get_current_buf()] and not luasnip.session.jump_active then
-				luasnip.unlink_current()
-			end
-		end,
-	})
-end
-
 return {
 	"hrsh7th/nvim-cmp",
 	config = true,
@@ -66,11 +37,6 @@ return {
 		},
 		-- add completion for package.json sort of similar to crates.nvim from rust
 		{ "David-Kunz/cmp-npm", ft = "json", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
-		{
-			"L3MON4D3/LuaSnip",
-			opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-			config = load_snippets,
-		},
 		{
 			"windwp/nvim-autopairs",
 			opts = { fast_wrap = {}, disable_filetype = { "TelescopePrompt", "vim" } },
--- snippets/README.md
+++ snippets/README.md
@@ -1,3 +1,4 @@
 # Snippets
 
+- [VSCode Snippet Generator](https://snippet-generator.app/?description=&tabtrigger=&snippet=&mode=vscode)
 - [How to use VSCode snippets with LuaSnip](https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#vs-code)
