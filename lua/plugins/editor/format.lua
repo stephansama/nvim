a82302f21869bc832d52656fc1f21331ff681a10
Stephan Randle <stephanrandle@hotmail.com>
2024-07-03 13:16:56 -0400

    updated prettier to just use prettier and not the daemon

--- lua/plugins/editor/format.lua
+++ lua/plugins/editor/format.lua
@@ -1,5 +1,5 @@
 local opts = {
-	format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	format_on_save = { timeout_ms = 1000, lsp_fallback = true },
 	--- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
 	formatters_by_ft = {
 		go = { "gofmt" },
@@ -10,14 +10,14 @@ local opts = {
 		yaml = { "yamlfmt" },
 		zshrc = { "shfmt" },
 		markdown = { "markdownlint" },
-		css = { { "prettierd", "prettier" } },
-		html = { { "prettierd", "prettier" } },
-		astro = { { "prettierd", "prettier" } },
-		svelte = { { "prettierd", "prettier" } },
-		javascript = { { "prettierd", "prettier" } },
-		typescript = { { "prettierd", "prettier" } },
-		javascriptreact = { { "prettierd", "prettier" } },
-		typescriptreact = { { "prettierd", "prettier" } },
+		css = { "prettier" },
+		html = { "prettier" },
+		astro = { "prettier" },
+		svelte = { "prettier" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
 	},
 }
 
