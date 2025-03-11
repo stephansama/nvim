cd30a29c4bf83d373f81a651860173826e681aec
stephansama <stephanrandle@hotmail.com>
2025-03-11 19:02:33 -0400

    chore: removed oil.nvim in favor of yazi

--- lua/plugins/editor/explorer.lua
+++ lua/plugins/editor/explorer.lua
@@ -1,35 +1,6 @@
 local keys = require("keys.init")
 
-local show_oil_detail = false
-
-local oil_opts = {
-	--- https://github.com/stevearc/oil.nvim?tab=readme-ov-file#options
-	default_file_explorer = false,
-	view_options = { show_hidden = true },
-	keymaps = {
-		["K"] = {
-			desc = "Toggle file detail view",
-			callback = function()
-				local oil = require("oil")
-				show_oil_detail = not show_oil_detail
-				oil.set_columns(vim.tbl_deep_extend("force", { "icon" }, show_oil_detail and {
-					"permissions",
-					"size",
-					"mtime",
-				} or {}))
-			end,
-		},
-	},
-}
-
 return {
-	{
-		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = true,
-		keys = keys.oil,
-		opts = oil_opts,
-	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		cmd = "Neotree",
