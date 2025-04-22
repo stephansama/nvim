d57551881ed502d626be1762a6398e0461993e42
stephansama <stephanrandle@hotmail.com>
2025-04-22 08:46:13 -0400

    chore: readded autopairs

--- lua/plugins/editor/cmp.lua
+++ lua/plugins/editor/cmp.lua
@@ -6,7 +6,17 @@ return {
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			keymap = { preset = "enter" },
+			keymap = {
+				preset = "enter",
+				["<CR>"] = {
+					function(cmp)
+						local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+						return cmp.accept({
+							callback = cmp_autopairs.on_confirm_done,
+						})
+					end,
+				},
+			},
 			appearance = { nerd_font_variant = "mono" },
 			cmdline = { completion = { menu = { auto_show = true } } },
 			completion = {
@@ -18,5 +28,10 @@ return {
 			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		opts_extend = { "sources.default" },
+	},
+	{
+		"windwp/nvim-autopairs",
+		opts = { fast_wrap = {}, disable_filetype = { "TelescopePrompt", "vim" } },
+		config = true,
 	},
 }
