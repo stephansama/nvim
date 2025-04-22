ce5d27c09d783f8380128a4cae91e91b3f7f3c5c
stephansama <stephanrandle@hotmail.com>
2025-04-22 01:14:26 -0400

    chore: removed builtin completion support in favor of plugin

--- lua/servers/init.lua
+++ lua/servers/init.lua
@@ -20,16 +20,16 @@ keys.default_lsp_keybindings(capabilities, on_attach)
 
 utils.load_lsp_configs(capabilities, on_attach)
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client then
-			if client:supports_method("textDocument/completion") then
-				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-			end
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+-- 	callback = function(ev)
+-- 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+-- 		if client then
+-- 			if client:supports_method("textDocument/completion") then
+-- 				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+-- 			end
+-- 		end
+-- 	end,
+-- })
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
