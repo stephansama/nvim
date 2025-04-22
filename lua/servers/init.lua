d3216ed1b6a2349479df37eb861680f726b92fea
stephansama <stephanrandle@hotmail.com>
2025-04-22 00:13:02 -0400

    chore: migrated to use lsp builtin

--- lua/servers/init.lua
+++ lua/servers/init.lua
@@ -21,6 +21,17 @@ keys.default_lsp_keybindings(capabilities, on_attach)
 utils.load_lsp_configs(capabilities, on_attach)
 
 vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client then
+			if client:supports_method("textDocument/completion") then
+				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+			end
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
