f105ed465c1a1681933dfb649837307cf3e23317
stephansama <stephanrandle@hotmail.com>
2025-05-04 21:23:24 -0400

    chore: updated debug plugin

---
+++ lua/plugins/debug/bloat.lua
@@ -0,0 +1,1 @@
+return { "dundalek/bloat.nvim", cmd = "Bloat" }
--- lua/plugins/debug/init.lua
+++ lua/plugins/debug/init.lua
@@ -1,13 +1,6 @@
 local keys = require("keys.init")
 
 return {
-	{ "dundalek/bloat.nvim", cmd = "Bloat" },
 	{ "dstein64/vim-startuptime", keys = keys.startuptime },
-	{
-		"meznaric/key-analyzer.nvim",
-		config = true,
-		keys = keys.key_analyzer,
-		opts = {},
-		cmd = "KeyAnalyzer",
-	},
+	{ "meznaric/key-analyzer.nvim", keys = keys.key_analyzer, config = true, cmd = "KeyAnalyzer" },
 }
