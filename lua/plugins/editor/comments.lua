4bc1316254ee805c700d3ffb299a9b5ed9bf281e
stephansama <stephanrandle@hotmail.com>
2025-05-04 21:16:31 -0400

    chore: refactored code

--- lua/plugins/editor/comments.lua
+++ lua/plugins/editor/comments.lua
@@ -1,28 +1,30 @@
 local ft = require("constants.ft").COMMENT_FILETYPES
 local keys = require("keys.init")
 
+local opts = function()
+	return {
+		--- LHS of operator-pending mappings in NORMAL and VISUAL mode
+		opleader = { line = "gc", block = "gC" },
+		toggler = { line = "gcc", block = "gCc" },
+		extra = { above = "gcO", below = "gco" },
+		pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+	}
+end
+
 return {
 	{
 		"folke/todo-comments.nvim",
-		event = "VeryLazy",
 		keys = keys.todo_comments,
+		event = "VeryLazy",
 		config = true,
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
 		"numToStr/Comment.nvim",
 		ft = ft,
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+		opts = opts,
 		config = true,
-		opts = function()
-			return {
-				--- LHS of operator-pending mappings in NORMAL and VISUAL mode
-				opleader = { line = "gc", block = "gC" },
-				toggler = { line = "gcc", block = "gCc" },
-				extra = { above = "gcO", below = "gco" },
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			}
-		end,
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 	},
 	{
 		"danymat/neogen",
