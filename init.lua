92e7d535e300a5d5e232eae328f4a1a68eddddfa
stephansama <stephanrandle@hotmail.com>
2025-08-06 14:30:00 -0400

    chore: removed neovide support

--- init.lua
+++ init.lua
@@ -2,12 +2,3 @@ require("srandle")
 
 ---> autocmd to underline all links <---
 vim.cmd([[autocmd BufEnter * match Underlined /https\?:\/\/\S\+/]])
-
-if vim.g.neovide then
-	local transparency = 0.5
-
-	vim.o.guifont = "FiraCode Nerd Font Mono:h22"
-	vim.g.transparency = transparency
-	vim.g.neovide_transparency = transparency
-	vim.g.neovide_window_blurred = true
-end
