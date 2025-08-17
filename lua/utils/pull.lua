1dfc213f496ae7bf5ca13ebbc83ea1519a26769a
stephansama <stephanrandle@hotmail.com>
2025-08-17 07:17:05 -0400

    chore: added cspell check for underneath .config folder

--- lua/utils/pull.lua
+++ lua/utils/pull.lua
@@ -3,16 +3,26 @@ local M = {}
 ---@param config_files table<string>
 M.config_file_exists = function(config_files)
 	local cwd = vim.fn.getcwd()
-	local ls_output = io.popen("ls -a " .. cwd, "r")
-	local config_exists = false
+	local cwd_output = io.popen("ls -a " .. cwd, "r")
+	local config_output = io.popen("ls -a " .. cwd .. "/.config", "r")
 
-	if ls_output then
-		for file in ls_output:lines() do
-			config_exists = config_exists or vim.tbl_contains(config_files, file)
+	if config_output then
+		for file in config_output:lines() do
+			if vim.tbl_contains(config_files, file) then
+				return true
+			end
 		end
 	end
 
-	return config_exists
+	if cwd_output then
+		for file in cwd_output:lines() do
+			if vim.tbl_contains(config_files, file) then
+				return true
+			end
+		end
+	end
+
+	return false
 end
 
 M.use_cspell = function()
