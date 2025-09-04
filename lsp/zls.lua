5c11938a8ebf711e542b4fddd13c92cb706bd498
stephansama <stephanrandle@hotmail.com>
2025-09-04 14:00:04 -0400

    chore: removed finding zls executable as mason is already in path

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,20 +1,9 @@
 --- https://zigtools.org/zls/install/
-local function get_zls()
-	return require("mason-registry").get_package("zls")
-end
-
-local path = pcall(get_zls)
-local zls = path and path or nil
-local zlsExecutable = zls and zls:get_latest_version() .. "/zls" or nil
 
 ---@class vim.lsp.Config
 local M = {}
 
-M.cmd = { zlsExecutable }
 M.ft = { "zig", "zir" }
 M.root_markers = { "zls.json", "build.zig", ".git" }
-M.settings = {
-	zls = { zig_exe_path = zlsExecutable },
-}
 
 return M
