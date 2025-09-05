d6370b6eac7180a049164076170eb9b223265f47
stephansama <stephanrandle@hotmail.com>
2025-09-04 22:06:45 -0400

    chore: readded conditional cspell toggle. consolidated utility files

--- lua/utils/lint.lua
+++ lua/utils/lint.lua
@@ -1,11 +1,39 @@
 local M = {}
 
-M.cspell_enabled = require("utils.pull").use_cspell()
+local function use_cspell(config_files)
+	local cwd = vim.fn.getcwd()
+
+	for _, file in ipairs(config_files) do
+		if vim.fn.filereadable(cwd .. "/" .. file) == 1 then
+			return true
+		end
+		if vim.fn.filereadable(cwd .. "/.config/" .. file) == 1 then
+			return true
+		end
+	end
+
+	return false
+end
+
+M.cspell_enabled =
+	use_cspell({
+		".cSpell.json",
+		".cspell.json",
+		"cSpell.json",
+		"cspell.config.cjs",
+		"cspell.config.js",
+		"cspell.config.json",
+		"cspell.config.yaml",
+		"cspell.config.yml",
+		"cspell.json",
+		"cspell.yaml",
+		"cspell.yml",
+	})
 
 M.lint = function()
 	local lint = require("lint")
 	lint.try_lint()
-	if M.cspell_enabled then
+	if M.cspell_enabled or _G.cspell_enabled then
 		lint.try_lint("cspell")
 	end
 end
--- lua/utils/lspsetup.lua
+++ lua/utils/lspsetup.lua
@@ -2,21 +2,124 @@ local lsp = require("utils.lsp")
 local SERVERS = require("plugins.lang").SERVERS
 local LSP_CONFIG_DIR = require("config.constants").LSP_CONFIG_DIR
 
-local lsp_config_output = io.popen("ls " .. LSP_CONFIG_DIR, "r")
+local config_files = vim.fn.readdir(LSP_CONFIG_DIR)
 
-if lsp_config_output then
-	for file in lsp_config_output:lines() do
-		local server = string.match(file, "(.+)%..+")
-		local config = dofile(LSP_CONFIG_DIR .. server .. ".lua")
-		vim.lsp.config(server, config)
+if config_files then
+	for _, entry in ipairs(config_files) do
+		local full_path = LSP_CONFIG_DIR .. entry
+		if vim.fn.filereadable(full_path) == 1 then
+			local server = string.match(entry, "(.+)%..+")
+			local config = dofile(full_path)
+			vim.lsp.config(server, config)
+		end
 	end
 end
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities =
+	vim.tbl_deep_extend(
+		"force",
+		capabilities,
+		require("blink.cmp").get_lsp_capabilities({}, false)
+	)
+capabilities = vim.tbl_deep_extend("force", capabilities, {
+	textDocument = {
+		foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		},
+	},
+})
+
 vim.cmd([[hi FloatShadow guifg=white guibg=#1f2335]])
 vim.cmd([[hi FloatShadowThrough guifg=white guibg=#1f2335]])
-vim.lsp.config("*", { capabilities = lsp.create_capabilities() })
+
+vim.lsp.config("*", { capabilities = capabilities })
 vim.lsp.enable(SERVERS)
 
 require("ufo").setup()
 
-lsp.setup_borders()
+--- LSP Border
+vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+vim.cmd(
+	[[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+)
+
+local border = require("utils.ui").border("FloatBorder")
+
+local handlers = {
+	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+		border = border,
+	}),
+	["textDocument/signatureHelp"] = vim.lsp.with(
+		vim.lsp.handlers.signature_help,
+		{ border = border }
+	),
+}
+
+for key, v in pairs(handlers) do
+	vim.lsp.handlers[key] = v
+end
+
+local diagnostic_source = "if_many"
+
+local diagnostics = require("icons.init").diagnostics
+
+vim.diagnostic.config({
+	float = {
+		border = border,
+		source = diagnostic_source,
+	},
+	virtual_text = { source = diagnostic_source },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = diagnostics.Error,
+			[vim.diagnostic.severity.WARN] = diagnostics.Warn,
+			[vim.diagnostic.severity.INFO] = diagnostics.Info,
+			[vim.diagnostic.severity.HINT] = diagnostics.Hint,
+		},
+		linehl = { [vim.diagnostic.severity.INFO] = "DiagnosticInfoMsg" },
+		numhl = { [vim.diagnostic.severity.WARN] = "WarningMsg" },
+	},
+})
+
+-- To instead override globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
+	opts = opts or {}
+	---@diagnostic disable-next-line: inject-field
+	opts.border = opts.border or border
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+local function lsp_hover()
+	local filetype = vim.bo.filetype
+	if vim.tbl_contains({ "vim", "help" }, filetype) then
+		vim.cmd("h " .. vim.fn.expand("<cword>"))
+	elseif vim.tbl_contains({ "man" }, filetype) then
+		vim.cmd("Man " .. vim.fn.expand("<cword>"))
+	elseif vim.fn.expand("%:t") == "package.json" then
+		local text = vim.api.nvim_get_current_line()
+		if not require("utils.init").is_package(text) then
+			return vim.lsp.buf.hover()
+		end
+		local match = text:match('"(.-)"')
+		local npm_link = require("config.constants").PACKAGE_JSON_URL .. match
+		require("utils").openURL(npm_link)
+	elseif vim.fn.expand("%:t") == "Cargo.toml" and require(
+		"crates"
+	).popup_available() then
+		require("crates").show_popup()
+	else
+		local winid = require("ufo").peekFoldedLinesUnderCursor()
+		if not winid then
+			vim.lsp.buf.hover()
+		end
+	end
+end
+
+if not _G.LspHoverCommandLoaded then
+	_G.LspHoverCommandLoaded = true
+	vim.api.nvim_create_user_command("LspHover", lsp_hover, { nargs = 0 })
+end
