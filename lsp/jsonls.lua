c8411d33414d13e5b6a3a8537f18f178ebab03a3
stephansama <stephanrandle@hotmail.com>
2025-09-09 06:50:24 -0400

    chore: added luarc to json schemas

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -1,6 +1,15 @@
+local extra = { {
+	description = "Luarc",
+	fileMatch = { "**/.luarc.json" },
+	name = "luarc",
+	url = "https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json",
+} }
+
 ---@type vim.lsp.Config
 return {
 	settings = {
-		json = { schemas = require("schemastore").json.schemas() },
+		json = {
+			schemas = require("schemastore").json.schemas({ extra = extra }),
+		},
 	},
 }
