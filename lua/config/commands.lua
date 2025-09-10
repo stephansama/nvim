be5a00cda26d6595f765c31fb2d81d73d922ad19
stephansama <stephanrandle@hotmail.com>
2025-09-10 06:56:45 -0400

    chore: refactored commands to use lua api instead of vimscript api

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,24 +1,3 @@
-local function reset_ui()
-	if package.preload["ufo"] then
-		vim.cmd([[UfoEnableFold]])
-	end
-
-	vim.cmd([[hi FloatBorder guifg=White guibg=none]])
-	vim.cmd([[hi LspInlayHint guifg=#646464 guibg=none]])
-	vim.cmd([[hi MiniIndentscopeSymbol guifg=#C93638 guibg=none]])
-	vim.cmd([[hi NoiceCmdlinePopupBorder guifg=#C93638 guibg=none]])
-	vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
-	vim.cmd(
-		[[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
-	)
-
-	if package.preload["dashboard"] then
-		vim.cmd([[ColorDashboard]])
-	end
-end
-
-vim.api.nvim_create_user_command("ResetUI", reset_ui, { nargs = 0 })
-
 --- highlight text on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
@@ -28,5 +7,53 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
---- highlight all links
-vim.cmd([[autocmd BufEnter * match Underlined /https\?:\/\/\S\+/]])
+--- underline found links
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("HighlightLinks", { clear = true }),
+	pattern = "*",
+	command = [[match Underlined /https\?:\/\/\S\+/]],
+})
+
+--- reset specific highlight groups when changing color schemes
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1f2335" })
+		vim.api.nvim_set_hl(0, "FloatBorder", {
+			fg = "white",
+			bg = "#1f2335",
+		})
+	end,
+})
+
+local function reset_ui()
+	if package.preload["ufo"] then
+		vim.cmd([[UfoEnableFold]])
+	end
+
+	vim.api.nvim_set_hl(0, "FloatBorder", {
+		fg = "White",
+		bg = "none",
+	})
+
+	vim.api.nvim_set_hl(0, "LspInlayHint", {
+		fg = "#646464",
+		bg = "none",
+	})
+
+	vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", {
+		fg = "#C93638",
+		bg = "none",
+	})
+
+	vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", {
+		fg = "#C93638",
+		bg = "none",
+	})
+
+	if package.preload["dashboard"] then
+		vim.cmd([[ColorDashboard]])
+	end
+end
+
+vim.api.nvim_create_user_command("ResetUI", reset_ui, { nargs = 0 })
