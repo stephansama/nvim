ba28b7e487dd84f9d9b6ae85475c15171fd97409
stephansama <stephanrandle@hotmail.com>
2025-09-17 13:32:30 -0400

    chore: add mise injection for toml

---
+++ after/queries/toml/injections.scm
@@ -0,0 +1,40 @@
+; https://mise.jdx.dev/mise-cookbook/neovim.html
+; modified to not use `is-mise` predicate (meaning it runs on every toml file)
+; extends
+
+(pair
+  (bare_key) @key (#eq? @key "run")
+  (string) @injection.content @injection.language
+
+  (#match? @injection.language "^['\"]{3}\n*#!(/\\w+)+/env\\s+\\w+") ; multiline shebang using env
+  (#gsub! @injection.language "^.*#!/.*/env%s+([^%s]+).*" "%1") ; extract lang
+  (#offset! @injection.content 0 3 0 -3) ; rm quotes
+)
+
+(pair
+  (bare_key) @key (#eq? @key "run")
+  (string) @injection.content @injection.language
+
+  (#match? @injection.language "^['\"]{3}\n*#!(/\\w+)+\s*\n") ; multiline shebang
+  (#gsub! @injection.language "^.*#!/.*/([^/%s]+).*" "%1") ; extract lang
+  (#offset! @injection.content 0 3 0 -3) ; rm quotes
+)
+
+(pair
+  (bare_key) @key (#eq? @key "run")
+  (string) @injection.content
+
+  (#match? @injection.content "^['\"]{3}\n*.*") ; multiline
+  (#not-match? @injection.content "^['\"]{3}\n*#!") ; no shebang
+  (#offset! @injection.content 0 3 0 -3) ; rm quotes
+  (#set! injection.language "bash") ; default to bash
+)
+
+(pair
+  (bare_key) @key (#eq? @key "run")
+  (string) @injection.content
+
+  (#not-match? @injection.content "^['\"]{3}") ; not multiline
+  (#offset! @injection.content 0 1 0 -1) ; rm quotes
+  (#set! injection.language "bash") ; default to bash
+)
