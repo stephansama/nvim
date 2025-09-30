65b71ae5e69dd45886d765fc9b84f179656effae
stephansama <stephanrandle@hotmail.com>
2025-09-30 18:00:52 -0400

    chore: removed even more from tailwindcss lsp spec as it is already merged

--- lsp/tailwindcss.lua
+++ lsp/tailwindcss.lua
@@ -1,15 +1,7 @@
 ---@type vim.lsp.Config
 local M = {}
 
-local original = require("lspconfig.configs.tailwindcss").default_config
-
 M.settings =
-	{
-		tailwindCSS = vim.tbl_deep_extend(
-			"force",
-			original.settings.tailwindCSS,
-			require("utils.tailwindcss").getLocalTailwindSettings()
-		),
-	}
+	{ tailwindCSS = require("utils.tailwindcss").getLocalTailwindSettings() }
 
-return vim.tbl_deep_extend("force", original, M)
+return M
