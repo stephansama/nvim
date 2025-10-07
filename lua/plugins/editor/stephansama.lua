bc12e95bb519022fcdf0fa4bb095eef814acce55
stephansama <stephanrandle@hotmail.com>
2025-10-07 03:01:36 -0400

    chore: updated fzf-nerdfont implementation

--- lua/plugins/editor/stephansama.lua
+++ lua/plugins/editor/stephansama.lua
@@ -1,20 +1,35 @@
 local keys = require("keys.plugin")
 
-vim.api.nvim_create_user_command(
-	"FzfTmuxMiseOrMake",
-	function()
-		if vim.fn.filereadable("mise.toml") == 1 then
-			vim.cmd([[FzfTmuxMise]])
-		elseif vim.fn.filereadable("Makefile") == 1 then
-			vim.cmd([[FzfTmuxMake]])
-		else
-			vim.notify([[No mise.toml or Makefile found]], vim.log.levels.WARN)
-		end
-	end,
-	{ nargs = 0 }
-)
+local fzf_tmux_mise_or_make = function()
+	if vim.fn.filereadable("mise.toml") == 1 then
+		vim.cmd([[FzfTmuxMise]])
+	elseif vim.fn.filereadable("Makefile") == 1 then
+		vim.cmd([[FzfTmuxMake]])
+	else
+		vim.notify([[No mise.toml or Makefile found]], vim.log.levels.WARN)
+	end
+end
+
+vim.api.nvim_create_user_command("FzfTmuxMiseOrMake", fzf_tmux_mise_or_make, {
+	nargs = 0,
+})
+
+local insert_fzf_nerdfont = function()
+	vim.cmd("stopinsert")
+	vim.cmd("FzfNerdfont")
+end
+
+vim.keymap.set("i", "<C-i>", insert_fzf_nerdfont, {
+	noremap = true,
+	silent = true,
+})
 
 return { {
+	"stephansama/fzf-nerdfont.nvim",
+	dir = vim.fn.expand("$CODE") .. "/nvim-plugins/fzf-nerdfont.nvim",
+	cmd = "FzfNerdfont",
+	keys = keys.fzf_nerdfont,
+}, {
 	"stephansama/stow.nvim",
 	cmd = "Stow",
 	keys = keys.stow,
