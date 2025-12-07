de4a007eaefb0e403e82c1663a45b2a7701a01ed
stephansama <stephanrandle@hotmail.com>
2025-12-07 13:34:49 -0500

    chore: reenabled .ts for prettier

--- lua/plugins/editor/conform.lua
+++ lua/plugins/editor/conform.lua
@@ -1,3 +1,5 @@
+local constants = require("config.constants")
+
 local function append_stop_flag(tbl)
 	for _, fmt_list in pairs(tbl) do
 		fmt_list.stop_after_first = true
@@ -19,7 +21,12 @@ return {
 				lsp_fallback = true,
 			},
 			formatters = {
-				prettier = { require_cwd = true },
+				prettier = {
+					require_cwd = true,
+					cwd = require("conform.util").root_file(
+						constants.PRETTIER_ROOT_CONFIG
+					),
+				},
 			},
 		}
 	end,
