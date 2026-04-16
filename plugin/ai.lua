38430c4295208af643309836ac33866547ce3643
stephansama <stephanrandle@hotmail.com>
2026-04-16 03:43:03 -0400

    chore: removed copilot and added *.json files to input

--- plugin/ai.lua
+++ plugin/ai.lua
@@ -1,24 +1,6 @@
 if not vim.tbl_contains(require("lang.enabled").enabled, "ai") then return end
 
-vim.pack.add({
-	{ src = "https://github.com/github/copilot.vim" },
-	{ src = "https://github.com/folke/sidekick.nvim" },
-})
-
-vim.g.copilot_no_maps = true
-
--- Block the normal Copilot suggestions
--- vim.api.nvim_create_augroup("github_copilot", { clear = true })
--- vim.api.nvim_create_autocmd(
--- 	{ "FileType", "BufUnload" },
--- 	{
--- 		group = "github_copilot",
--- 		callback = function(args)
--- 			vim.fn["copilot#On" .. args.event]()
--- 		end,
--- 	}
--- )
-vim.fn["copilot#OnFileType"]()
+vim.pack.add({ { src = "https://github.com/folke/sidekick.nvim" } })
 
 require("sidekick").setup({
 	cli = {
