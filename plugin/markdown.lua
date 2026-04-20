d2a6908dbb58839d199d887a569abe8ffa4b2c00
stephansama <stephanrandle@hotmail.com>
2026-04-20 00:15:03 -0400

    fix(markdown): updated markdown preview build hook

--- plugin/markdown.lua
+++ plugin/markdown.lua
@@ -3,32 +3,20 @@ if not vim.tbl_contains(
 	"markdown"
 ) then return end
 
+vim.g.table_mode_tableize_map = "<leader>mc"
+
+vim.api.nvim_create_autocmd("PackChanged", { callback = function(ev)
+	local name, kind = ev.data.spec.name, ev.data.kind
+	local is_valid = kind == "install" or kind == "update"
+	if name == "markdown-preview.nvim" and is_valid then
+		vim.cmd.packadd("markdown-preview.nvim")
+		vim.fn["mkdp#util#install"]()
+		vim.print(vim.inspect("installing markdown preview"))
+	end
+end })
+
 vim.pack.add({
 	{ src = "https://github.com/dhruvasagar/vim-table-mode" },
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 	{ src = "https://github.com/iamcco/markdown-preview.nvim" },
 })
-
-return { {
-	"dhruvasagar/vim-table-mode",
-	ft = "markdown",
-	config = function() end,
-	keys = require("keys.plugin").table_mode,
-	init = function()
-		vim.g.table_mode_tableize_map = "<leader>mc"
-	end,
-}, {
-	"MeanderingProgrammer/render-markdown.nvim",
-	ft = { "markdown", "markdown.mdx" },
-	opts = {},
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
-	},
-}, {
-	"iamcco/markdown-preview.nvim",
-	ft = { "markdown" },
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
-	keys = require("keys.plugin").markdownpreview,
-} }
